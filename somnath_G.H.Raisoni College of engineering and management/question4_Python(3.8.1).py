6cc0f36afdf9bec9ea028c727c6637110ad0946c
somnath chouwdhury <somnathchouwdhury7@gmail.com>
2021-06-23 18:37:54 +0530

    Update question4_Python(3.8.1).py

--- somnath_G.H.Raisoni College of engineering and management/question4_Python(3.8.1).py
+++ somnath_G.H.Raisoni College of engineering and management/question4_Python(3.8.1).py
@@ -1,1 +1,38 @@
 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 23 17:53:16 2021
+
+@author: samac
+"""
+            
+    
+    
+    
+    
+    
+    
+def main():
+    n=int(input())
+    res=1
+    s=list(map(int,input().split()))
+    
+    q=int(input())
+        
+    for i in range(q):
+        st=s
+        l,r=map(int,input().split())
+        li=s.index(l)
+        ri=s.index(r)
+        
+        a=st.pop(li-1)
+        b=st.pop(ri-1)
+        r=1
+        for f in(st):
+            r+=a^f * b^f
+            print(r)
+        res+=r*(a^b)
+    print(res)
+    
+main()
